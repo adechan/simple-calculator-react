236cab93f502419a8c16f5d2fb62b86a4ebfb97d
adechan <ade.enter@yahoo.com>
2020-06-23 21:46:16 +0300

    Updates

---
+++ precache-manifest.c4ab8a1074243400d2c09b339c08e8b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5cd3b6556224b9497cb4eca3ca1e4f",
+    "url": "/Calculator-React/public/index.html"
+  },
+  {
+    "revision": "a805be0d4f65de2632c6",
+    "url": "/Calculator-React/public/static/css/main.2b262fa1.chunk.css"
+  },
+  {
+    "revision": "039b9c45f2666099685b",
+    "url": "/Calculator-React/public/static/js/2.6033af1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator-React/public/static/js/2.6033af1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a805be0d4f65de2632c6",
+    "url": "/Calculator-React/public/static/js/main.dba17f31.chunk.js"
+  },
+  {
+    "revision": "3240f5bf7c662a55c050",
+    "url": "/Calculator-React/public/static/js/runtime-main.f2a26912.js"
+  }
+]);
